6fb65e4ae37338c043b94b193175d79ca0452374
Davide <dignazio90@gmail.com>
2024-08-17 18:29:35 +0200

    Selfwork Cicli 1

---
+++ javascript 3/main3.js
@@ -0,0 +1,9 @@
+// SELFWORK CICLI 1
+
+let num = 2;  
+let limite = 10; 
+
+console.log(`Tabellina del ${num}:`);
+for (let i = 1; i <= limite; i++) {
+    console.log(`${num} x ${i} = ${num * i}`);
+}
